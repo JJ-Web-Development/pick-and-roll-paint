796c552163b75480be935ca3311358bc6e37a2d6
Justin Golden <justingolden21@gmail.com>
2020-06-17 22:56:18 -0700

    many css updates, redesigned hero

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $( ()=> {
-	console.log('hello');
+	$('#hero').css('height', window.innerHeight);
+
+	// @TODO: contact btn onclick focus name input in form
+
+	$('#contact-link').mouseover(function() {
+		$(this).html('<i class="fas fa-envelope-open"></i> Contact');
+	});
+	$('#contact-link').mouseout(function() {
+		$(this).html('<i class="fas fa-envelope"></i> Contact');
+	});
+
+	$('#contact-btn').mouseover(function() {
+		$(this).html('<i class="fas fa-envelope-open"></i> &nbsp; Contact');
+	});
+	$('#contact-btn').mouseout(function() {
+		$(this).html('<i class="fas fa-envelope"></i> &nbsp; Contact');
+	});
 });
